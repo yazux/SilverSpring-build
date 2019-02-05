6a220e962422d61bbdbdce48502a446cff3836da
yazu <speed.live@mail.ru>
2019-02-06 01:30:46 +1000

    Попытка заставить работать mobile accessibility

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,115 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-console.console",
+    "file": "plugins/cordova-plugin-console/www/console-via-logger.js",
+    "pluginId": "cordova-plugin-console",
+    "clobbers": [
+      "console"
+    ]
+  },
+  {
+    "id": "cordova-plugin-console.logger",
+    "file": "plugins/cordova-plugin-console/www/logger.js",
+    "pluginId": "cordova-plugin-console",
+    "clobbers": [
+      "cordova.logger"
+    ]
+  },
+  {
+    "id": "cordova-plugin-device.device",
+    "file": "plugins/cordova-plugin-device/www/device.js",
+    "pluginId": "cordova-plugin-device",
+    "clobbers": [
+      "device"
+    ]
+  },
+  {
+    "id": "cordova-plugin-device-orientation.CompassError",
+    "file": "plugins/cordova-plugin-device-orientation/www/CompassError.js",
+    "pluginId": "cordova-plugin-device-orientation",
+    "clobbers": [
+      "CompassError"
+    ]
+  },
+  {
+    "id": "cordova-plugin-device-orientation.CompassHeading",
+    "file": "plugins/cordova-plugin-device-orientation/www/CompassHeading.js",
+    "pluginId": "cordova-plugin-device-orientation",
+    "clobbers": [
+      "CompassHeading"
+    ]
+  },
+  {
+    "id": "cordova-plugin-device-orientation.compass",
+    "file": "plugins/cordova-plugin-device-orientation/www/compass.js",
+    "pluginId": "cordova-plugin-device-orientation",
+    "clobbers": [
+      "navigator.compass"
+    ]
+  },
+  {
+    "id": "cordova-plugin-dialogs.notification",
+    "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+    "pluginId": "cordova-plugin-dialogs",
+    "merges": [
+      "navigator.notification"
+    ]
+  },
+  {
+    "id": "cordova-plugin-inappbrowser.inappbrowser",
+    "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+    "pluginId": "cordova-plugin-inappbrowser",
+    "clobbers": [
+      "cordova.InAppBrowser.open",
+      "window.open"
+    ]
+  },
+  {
+    "id": "cordova-plugin-network-information.network",
+    "file": "plugins/cordova-plugin-network-information/www/network.js",
+    "pluginId": "cordova-plugin-network-information",
+    "clobbers": [
+      "navigator.connection",
+      "navigator.network.connection"
+    ]
+  },
+  {
+    "id": "cordova-plugin-network-information.Connection",
+    "file": "plugins/cordova-plugin-network-information/www/Connection.js",
+    "pluginId": "cordova-plugin-network-information",
+    "clobbers": [
+      "Connection"
+    ]
+  },
+  {
+    "id": "phonegap-plugin-mobile-accessibility.mobile-accessibility",
+    "file": "plugins/phonegap-plugin-mobile-accessibility/www/mobile-accessibility.js",
+    "pluginId": "phonegap-plugin-mobile-accessibility",
+    "clobbers": [
+      "window.MobileAccessibility"
+    ]
+  },
+  {
+    "id": "phonegap-plugin-mobile-accessibility.MobileAccessibilityNotifications",
+    "file": "plugins/phonegap-plugin-mobile-accessibility/www/MobileAccessibilityNotifications.js",
+    "pluginId": "phonegap-plugin-mobile-accessibility",
+    "clobbers": [
+      "MobileAccessibilityNotifications"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-console": "1.0.7",
+  "cordova-plugin-device": "2.0.3-dev",
+  "cordova-plugin-device-orientation": "1.0.7",
+  "cordova-plugin-dialogs": "1.2.1",
+  "cordova-plugin-inappbrowser": "1.3.0",
+  "cordova-plugin-network-information": "2.0.2-dev",
+  "cordova-plugin-whitelist": "1.2.2",
+  "phonegap-plugin-mobile-accessibility": "1.0.5-dev"
+};
+// BOTTOM OF METADATA
+});
